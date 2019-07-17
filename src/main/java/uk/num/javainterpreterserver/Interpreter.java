487bd590649711c8f6fde5897f3b91f38cc66b00
Tony Walmsley <tony@aosd.co.uk>
2019-07-17 11:38:53 +0100

    Initial Version

---
+++ src/main/java/uk/num/javainterpreterserver/Interpreter.java
@@ -0,0 +1,35 @@
+package uk.num.javainterpreterserver;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import uk.modl.modlObject.ModlObject;
+import uk.modl.parser.printers.JsonPrinter;
+
+@RestController
+public class Interpreter {
+
+    @GetMapping("*")
+    public String interpretGet(@RequestParam("modl") final String modl) {
+        return handler(modl);
+    }
+
+    @PostMapping("*")
+    public String interpretPost(@RequestBody final String modl) {
+        return handler(modl);
+    }
+
+    private String handler(final String modl) {
+        ModlObject modlObject = null;
+        try {
+            if (modl != null) {
+                modlObject = uk.modl.interpreter.Interpreter.interpret(modl);
+            }
+
+            return JsonPrinter.printModl(modlObject);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Error processing the supplied MODL string", e);
+        }
+
+    }
+}
