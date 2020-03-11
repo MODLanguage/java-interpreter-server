76e214c10383b13146823c11f4deaae26d1232b1
Tony Walmsley <tony@aosd.co.uk>
2020-03-11 11:23:53 +0000

    Add Eureka.

--- src/main/java/uk/num/javainterpreterserver/Main.java
+++ src/main/java/uk/num/javainterpreterserver/Main.java
@@ -5,12 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(exclude = {
         DataSourceAutoConfiguration.class,
         DataSourceTransactionManagerAutoConfiguration.class,
         HibernateJpaAutoConfiguration.class
 })
+@EnableDiscoveryClient
+@EnableEurekaClient
 public class Main {
 
     public static void main(String[] args) {
