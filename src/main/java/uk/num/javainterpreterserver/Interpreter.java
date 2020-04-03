04f0b1041478831b8622c0dae5e1bfbb13b76eb7
Tony Walmsley <tony@aosd.co.uk>
2020-04-03 19:02:47 +0100

    Add non-CORS services for internal use and CORS versions for external use.

--- src/main/java/uk/num/javainterpreterserver/Interpreter.java
+++ src/main/java/uk/num/javainterpreterserver/Interpreter.java
@@ -9,11 +9,13 @@ import uk.modl.parser.printers.JsonPrinter;
 @RestController
 public class Interpreter {
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/")
     public String interpretGet(@RequestParam("modl") final String modl) {
         return handler(modl);
     }
 
+    @CrossOrigin(origins = "*")
     @PostMapping("/")
     public String interpretPost(@RequestBody final String modl) {
         return handler(modl);
@@ -36,4 +38,15 @@ public class Interpreter {
         }
 
     }
+
+    @GetMapping("/internal/")
+    public String interpretInternalGet(@RequestParam("modl") final String modl) {
+        return handler(modl);
+    }
+
+    @PostMapping("/internal/")
+    public String interpretInternalPost(@RequestBody final String modl) {
+        return handler(modl);
+    }
+
 }
