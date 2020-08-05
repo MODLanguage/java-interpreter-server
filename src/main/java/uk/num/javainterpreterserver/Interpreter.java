fe9da4e91b8d3a7b0a318aa2360897fdcc212613
Tony Walmsley <tony@aosd.co.uk>
2020-08-05 08:05:51 +0100

    Limit NUM Records to 25500 characters in the Java Interpreter Server.

--- src/main/java/uk/num/javainterpreterserver/Interpreter.java
+++ src/main/java/uk/num/javainterpreterserver/Interpreter.java
@@ -1,6 +1,7 @@
 package uk.num.javainterpreterserver;
 
 import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -8,6 +9,8 @@ import org.springframework.web.server.ResponseStatusException;
 @RestController
 @Log4j2
 public class Interpreter {
+
+    public final int MAX_NUM_RECORD_LEN = 25500;
 
     private final uk.modl.interpreter.Interpreter interpreter = new uk.modl.interpreter.Interpreter();
 
@@ -28,7 +31,10 @@ public class Interpreter {
         try {
             if (modl != null) {
                 final long start = System.currentTimeMillis();
-                jsonString = interpreter.interpretToJsonString(modl);
+
+                final String truncated = StringUtils.truncate(modl, MAX_NUM_RECORD_LEN);
+
+                jsonString = interpreter.interpretToJsonString(truncated);
                 final long end = System.currentTimeMillis();
                 log.info(String.format("Took %dms to interpret %s", (end - start), modl));
             }
