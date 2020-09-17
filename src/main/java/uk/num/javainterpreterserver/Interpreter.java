e0145cb0251cc10d5af7dab52048bbe1c39eb681
Tony Walmsley <tony@aosd.co.uk>
2020-09-17 11:08:20 +0100

    Add a timeout for complex MODL.

--- src/main/java/uk/num/javainterpreterserver/Interpreter.java
+++ src/main/java/uk/num/javainterpreterserver/Interpreter.java
@@ -2,15 +2,20 @@ package uk.num.javainterpreterserver;
 
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import uk.modl.parser.errors.InterpreterError;
 
 @RestController
 @Log4j2
 public class Interpreter {
 
     public static final int MAX_WIDTH = 100;
+
+    @Value("${interpreter.timeoutMilliseconds}")
+    private int timeoutMilliseconds = 5000;
 
     public final int MAX_NUM_RECORD_LEN = 25500;
 
@@ -36,7 +41,7 @@ public class Interpreter {
 
                 final String truncated = StringUtils.truncate(modl, MAX_NUM_RECORD_LEN);
 
-                jsonString = interpreter.interpretToJsonString(truncated, null);
+                jsonString = interpreter.interpretToJsonString(truncated, null, timeoutMilliseconds);
                 final long end = System.currentTimeMillis();
                 log.info(String.format("Took %dms to interpret %s", (end - start), StringUtils.truncate(modl, MAX_WIDTH) + "..."));
             }
@@ -46,11 +51,16 @@ public class Interpreter {
             } else {
                 return "";
             }
+        } catch (final InterpreterError | Error e) {
+            if (e.getMessage()
+                    .equals("Interpreter Error: java.util.concurrent.TimeoutException")) {
+                log.info("Aborting the request because it was taking too long to process: '{}'", StringUtils.truncate(modl, MAX_WIDTH) + "...");
+            } else {
+                log.info("Aborting the request because a serious error has occurred interpreting this: '{}'", StringUtils.truncate(modl, MAX_WIDTH) + "...");
+            }
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Internal Error processing the supplied MODL string. Try simplifying your MODL.");
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Error processing the supplied MODL string: " + e);
-        } catch (final Error e) {
-            log.error("Aborting the request because a serious error has occurred interpreting this: '{}'", StringUtils.truncate(modl, MAX_WIDTH) + "...");
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Internal Error processing the supplied MODL string. Try simplifying your MODL.");
         }
 
     }
