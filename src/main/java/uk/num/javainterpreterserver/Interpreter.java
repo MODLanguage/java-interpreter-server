188dc13de11fdbbaea15c46b134d2f140c2a9f81
Tony Walmsley <tony@aosd.co.uk>
2021-01-28 10:07:24 +0000

    Update interpreter library - prevent recursive `*load`

--- src/main/java/uk/num/javainterpreterserver/Interpreter.java
+++ src/main/java/uk/num/javainterpreterserver/Interpreter.java
@@ -1,13 +1,20 @@
 package uk.num.javainterpreterserver;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import lombok.Setter;
-import lombok.extern.log4j.Log4j2;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j2;
 import uk.modl.parser.errors.InterpreterError;
 
 @RestController
